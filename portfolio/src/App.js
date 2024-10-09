b202ae0b78eee2c1d5fe4a4ffd0c2d2d67f7566d
David Aydin <david.aydin6@gmail.com>
2024-10-09 11:53:51 -0400

    Added react-router-dom and created Home Page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,28 +1,45 @@
 // Dependencies
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 // Styles
 import './assets/styles/App.css';
 
+// Pages
+import Home from './pages/Home';
+
 function App() {
+  const companyName = 'daydin14';
+  const year = new Date().getFullYear();
+  const rights = 'All rights reserved.';
+  const author = 'David Aydin';
+
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-          </ul>
-        </nav>
+    <Router>
+      <div >
+        {/* Header */}
+        <header>
+          <h1>David Aydin</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+            </ul>
+          </nav>
+        </header>
+        {/* Main */}
         <main>
-          <h1>Welcome to my portfolio!</h1>
-          <p>Here is some information about me.</p>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+          </Routes>
         </main>
+        {/* Footer */}
         <footer>
-          <p>&copy; 2024</p>
+          <p>{companyName} &copy; {year} {rights}</p>
+          <p>Created by {author}</p>
         </footer>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
 
