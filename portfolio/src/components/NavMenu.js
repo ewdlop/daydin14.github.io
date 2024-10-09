3996b6de17b91bc3e19213c8d0d641f3529f091f
David Aydin <david.aydin6@gmail.com>
2024-10-09 16:52:48 -0400

    Moved Theme Toggle to NavMenu

--- portfolio/src/components/NavMenu.js
+++ portfolio/src/components/NavMenu.js
@@ -2,16 +2,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+// Components
+import ThemeToggle from './ThemeToggle';
+
 const NavMenu = () => {
     return (
         <>
-            <nav className="bg-gray-200 dark:bg-gray-800 shadow-lg">
-                <ul className="menu menu-horizontal p-0">
+            <nav className="bg-gray-200 dark:bg-gray-800 shadow-lg rounded-lg">
+                <ul className="menu menu-horizontal p-2">
                     <li><Link to="/" className="btn btn-ghost text-black dark:text-white">Home</Link></li>
                     <li><Link to="/about" className="btn btn-ghost text-black dark:text-white">About</Link></li>
                     {process.env.NODE_ENV === 'development' && (
                         <li><Link to="/sandbox" className="btn btn-ghost text-black dark:text-white">Sandbox</Link></li>
                     )}
+                    <li>
+                        <ThemeToggle />
+                    </li>
                 </ul>
             </nav>
         </>
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,13 +2,11 @@
 import React from 'react'
 
 // Components
-import ThemeToggle from '../components/ThemeToggle'
 
 const Home = () => {
     return (
         <div className="flex flex-col items-center justify-center bg-white dark:bg-gray-800 text-black dark:text-white">
             <h1 className="text-4xl font-bold mb-4">Home</h1>
-            <ThemeToggle />
         </div>
     )
 }
