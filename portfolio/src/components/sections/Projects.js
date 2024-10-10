6eb8dc91442d4311abda59f84504ebe81ef90674
David Aydin <david.aydin6@gmail.com>
2024-10-10 13:20:21 -0400

    Refactor Projects component to improve project card layout

--- portfolio/src/components/sections/Projects.js
+++ portfolio/src/components/sections/Projects.js
@@ -41,26 +41,40 @@ const Projects = () => {
                 </ul>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-4 w-full max-w-6xl mx-auto">
+            <div className="flex justify-center items-center mb-4">
+                {selectedCategory === 'Udemy' && (
+                    <>
+                        <h2 className="text-2xl font-semibold text-center mr-4">Udemy Projects</h2>
+                        <a href='https://github.com/daydin14/udemy' target='_blank' rel='noopener noreferrer'>
+                            <img src={GitHubMark} alt="GitHub" className="w-8 h-8 dark:hidden" />
+                            <img src={GitHubMarkWhite} alt="GitHub" className="w-8 h-8 hidden dark:block" />
+                        </a>
+                    </>
+                )}
+            </div>
+
+            <div className="flex flex-wrap justify-center gap-4 mb-4 w-full max-w-6xl mx-auto">
                 {filteredProjects.map((project, index) => (
-                    <div key={index} className="card bg-gray-200 dark:bg-gray-800 shadow-xl h-100 overflow-hidden">
+                    <div key={index} className="card bg-gray-200 dark:bg-gray-800 shadow-xl h-100 overflow-hidden w-80">
                         <figure className="h-32 overflow-hidden">
                             <img src={project.image} alt={project.title} className="object-cover w-full h-full" />
                         </figure>
                         <div className="card-body p-4">
                             <h3 className="card-title text-lg font-semibold truncate">{project.title}</h3>
                             <p className="text-sm text-gray-700 dark:text-gray-300 overflow-hidden overflow-ellipsis">{project.shortDescription}</p>
-                            {project.githubRepoLink && (
-                                <a href={project.githubRepoLink} target='_blank' rel='noopener noreferrer'>
-                                    <img src={GitHubMark} alt="GitHub" className="w-8 h-8 dark:hidden" />
-                                    <img src={GitHubMarkWhite} alt="GitHub" className="w-8 h-8 hidden dark:block" />
-                                </a>
-                            )}
-                            {project.liveDemoLink && (
-                                <a href={project.liveDemoLink} target='_blank' rel='noopener noreferrer' className="text-blue-500 hover:underline flex items-center">
-                                    <i className="fas fa-play ml-2"></i>
-                                </a>
-                            )}
+                            <div className='flex flex-row justify-end items-center p-2'>
+                                {project.githubRepoLink && (
+                                    <a href={project.githubRepoLink} target='_blank' rel='noopener noreferrer' className='mr-2'>
+                                        <img src={GitHubMark} alt="GitHub" className="w-8 h-8 dark:hidden" />
+                                        <img src={GitHubMarkWhite} alt="GitHub" className="w-8 h-8 hidden dark:block" />
+                                    </a>
+                                )}
+                                {project.liveDemoLink && (
+                                    <a href={project.liveDemoLink} target='_blank' rel='noopener noreferrer' className="text-blue-500 hover:underline flex items-center">
+                                        <i className="fas fa-play ml-2"></i>
+                                    </a>
+                                )}
+                            </div>
                         </div>
                     </div>
                 ))}
